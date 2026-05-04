e76f9faf073f1756b6efd0cedd593a2aa8f1af26
Arteemyev Mikhail <artemevmik1@tambler>
2026-05-04 08:23:03 +0500

    init

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+CURRENT_FILE = Path(__file__).resolve()
+print(CURRENT_FILE)
+ROOT_DIR = CURRENT_FILE.parent
+
+NAMES_TXT = ROOT_DIR / "data/names.txt"
---
+++ src/main.py
@@ -0,0 +1,21 @@
+from config import NAMES_TXT
+
+def clear_names (file_name: str) -> list:
+    """ функция, для оочистки имен от лишгних символов """
+    new_names_list = list()
+    with open(file_name, 'r', encoding='utf-8') as names_file:
+        names_list = names_file.read().split()
+        for name_item in names_list:
+            new_name = ''
+            for symbol in name_item:
+                if symbol.isalpha():
+                    new_name += symbol
+            if new_name.isalpha():
+                new_names_list.append(new_name)
+    return new_names_list
+
+if __name__ == '__main__':
+    cleared_name = clear_names(NAMES_TXT)
+
+    for i in cleared_name:
+        print(i)
