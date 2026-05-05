cef116b16f62545da1bf48e6bcf9734cb15d10c6
Arteemyev Mikhail <artemevmik1@tambler>
2026-05-05 09:26:56 +0500

    black fixes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import re
 from config import NAMES_TXT
 
-def clear_names (file_name: str) -> list:
-    """ функция, для оочистки имен от лишгних символов """
+
+def clear_names(file_name: str) -> list:
+    """функция, для оочистки имен от лишгних символов"""
     new_names_list = list()
-    with open(file_name, 'r', encoding='utf-8') as names_file:
+    with open(file_name, "r", encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
-            new_name = ''
+            new_name = ""
             for symbol in name_item:
                 if symbol.isalpha():
                     new_name += symbol
@@ -16,13 +17,12 @@ def clear_names (file_name: str) -> list:
     return new_names_list
 
 
+def is_cyrillic(name_item: str, pattern=None) -> bool:
+    """проверкка на вхождение кириллицы в строку"""
+    return bool(re.search("[а-яА-Я]", name_item))
 
-def is_cyrillic (name_item: str, pattern=None) -> bool:
-    """ проверкка на вхождение кириллицы в строку"""
-    return bool(re.search('[а-яА-Я]', name_item))
 
-
-def filter_russian_names (names_list: list) -> list:
+def filter_russian_names(names_list: list) -> list:
     """фильтрация имен написанных на русском"""
     new_names_list = list()
     for name_item in names_list:
@@ -31,7 +31,7 @@ def filter_russian_names (names_list: list) -> list:
     return new_names_list
 
 
-def filter_english_names (names_list: list) -> list:
+def filter_english_names(names_list: list) -> list:
     """фильтрация имен написанных на русском"""
     new_names_list = list()
     for name_item in names_list:
@@ -42,16 +42,18 @@ def filter_english_names (names_list: list) -> list:
 
 def save_to_file(file_name: str, data: str) -> None:
     """Сохраняет данные в файл"""
-    with open(file_name, 'w', encoding='utf-8') as names_file:
+    with open(file_name, "w", encoding="utf-8") as names_file:
         names_file.write(data)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     cleared_name = clear_names(NAMES_TXT)
 
-    #for i in cleared_name:
-     #   print(i)
+    # for i in cleared_name:
+    #   print(i)
 
     filtered_names = filter_russian_names(cleared_name)
-    save_to_file('russian_names.txt', '\n'.join(filtered_names))
+    save_to_file("russian_names.txt", "\n".join(filtered_names))
 
     filtered_names = filter_english_names(cleared_name)
-    save_to_file('english_names.txt', '\n'.join(filtered_names))
+    save_to_file("english_names.txt", "\n".join(filtered_names))
